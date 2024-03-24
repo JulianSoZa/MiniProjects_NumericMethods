0505e9c42c06cba195bd31f5ca4a826bfcbd0f23
JulianSoZa <juliansoza19@gmail.com>
2024-03-24 13:02:03 -0500

    Cambio de enrutacion

--- MiniProject2_GP2/main.py
+++ MiniProject2_GP2/main.py
@@ -1,12 +1,13 @@
 from modules import electric_field, electric_potential, plot_electric_solution
-from modules.domainDiscretization import cartesian_discretization, polar_discretization
+from modules.domainDiscretization import cartesian as doCartesian 
+from modules.domainDiscretization import polar as doPolar
 
 nx = 10
 ny = 20
 r_inf = 3
 r_sup = 8
-elemento, puntos = cartesian_discretization.cartesian_discretization(nx, ny, r_inf, r_sup)
+elemento, puntos = doCartesian.cartesian_discretization(nx, ny, r_inf, r_sup)
 
 nth = 30
 nr = 30
-t_dis, r_dis, th, r, x_s, y_s = polar_discretization.polar_discretization(nth, nr)
+t_dis, r_dis, th, r, x_s, y_s = doPolar.polar_discretization(nth, nr)
